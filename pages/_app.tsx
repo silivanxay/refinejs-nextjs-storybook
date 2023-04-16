889aa9d13413e3bfe526e57e18efba400fde368d
silivanxay <silivanxay@gmail.com>
2023-04-16 23:37:33 +0700

    initial commit with superplate

---
+++ pages/_app.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { AppProps } from "next/app";
+import type { NextPage } from "next";
+import { Refine, GitHubBanner } from "@refinedev/core";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import routerProvider, {
+  UnsavedChangesNotifier,
+} from "@refinedev/nextjs-router";
+
+import dataProvider from "@refinedev/simple-rest";
+import { Layout } from "@components/layout";
+import { Header } from "@components/header";
+
+const API_URL = "https://api.fake-rest.refine.dev";
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  noLayout?: boolean;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout): JSX.Element {
+  const renderComponent = () => {
+    if (Component.noLayout) {
+      return <Component {...pageProps} />;
+    }
+
+    return <Component {...pageProps} />;
+  };
+
+  return (
+    <>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <Refine
+          routerProvider={routerProvider}
+          dataProvider={dataProvider(API_URL)}
+          resources={[
+            {
+              name: "blog_posts",
+              list: "/blog-posts",
+              create: "/blog-posts/create",
+              edit: "/blog-posts/edit/:id",
+              show: "/blog-posts/show/:id",
+              meta: {
+                canDelete: true,
+              },
+            },
+            {
+              name: "categories",
+              list: "/categories",
+              create: "/categories/create",
+              edit: "/categories/edit/:id",
+              show: "/categories/show/:id",
+              meta: {
+                canDelete: true,
+              },
+            },
+          ]}
+          options={{
+            syncWithLocation: true,
+            warnWhenUnsavedChanges: true,
+          }}
+        >
+          {renderComponent()}
+          <RefineKbar />
+          <UnsavedChangesNotifier />
+        </Refine>
+      </RefineKbarProvider>
+    </>
+  );
+}
+
+export default MyApp;
